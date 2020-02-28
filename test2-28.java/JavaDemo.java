1cce323b9c848b024e604dabfd531e20dd5e6db9
zhengyudong0430 <2238642137@qq.com>
2020-02-28 18:28:47 +0800

    基础练习

---
+++ test2-28.java/JavaDemo.java
@@ -0,0 +1,21 @@
+public class JavaDemo {
+  public static void main(String[]args) {
+      /*int n=10;
+      System.out.println("n="+n);
+       
+     long n=10L;
+     System.out.println("n="+n);
+     char c='b';
+     System.out.println(c);
+     int d=c;
+     System.out.println(d);
+    
+       System.out.println(Integer.MAX_VALUE+1);
+       */
+     int a=10;
+     int b=20;
+     System.out.println("a+b="+(a+b));
+     String s="\"hahaha\"";
+     System.out.println(s);
+      }
+}
