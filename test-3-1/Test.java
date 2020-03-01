ccceaa1bbf6ed07539bb02b343f7cac0230d1da7
zhengyudong0430 <2238642137@qq.com>
2020-03-01 23:01:33 +0800

    代码练习

---
+++ test-3-1/Test.java
@@ -0,0 +1,26 @@
+public class Test {
+
+    public static void main(String[] args) {
+     int  a = 3; 
+     byte b = a;
+      //byte a = 130;
+      //  float b = 3.5; 
+
+		///double x=2.0;
+		//int y=4;
+		// x/=++y;
+		//short a =128;
+
+        //byte b =(byte) a;
+        //System.out.println(a);
+		System.out.println(a);
+		System.out.println(b);
+	//public static void main1(String[] args) {
+		//在屏幕上打印出   "hello"  \  ->转义  \"==>"
+		//
+	//	System.out.println("\\\"hello\\\"");
+}
+}
+
+
+
