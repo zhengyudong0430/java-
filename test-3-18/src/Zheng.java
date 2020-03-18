db229ad0f817c2f7444667c73ef379e8dee7b584
zhengyudong0430 <2238642137@qq.com>
2020-03-18 23:05:11 +0800

    java经典习题

---
+++ test-3-18/src/Zheng.java
@@ -0,0 +1,55 @@
+public class Zheng {
+    public static void main(String[] args) {
+        for(int 1ioj)
+    }
+        /*public static void main(String[] args) {
+           //int count=0;
+            int j=0;
+            for(int i=11;i<20;i+=2) {
+               //boolean flag=true;
+                for(j=2;j<=Math.sqrt(i);j++) {
+                    if(i%j==0) {
+                        //flag=false;
+                        break;
+                    }
+                }
+                if(j>Math.sqrt(i)) {
+                    //count++;
+                    System.out.println(i);
+                }
+            }
+            //System.out.println(count);
+        }*/
+    }
+
+
+        /*for(int i=101;i<200;i++) {
+            boolean flg=true;
+            for(int j=2;j<i;j++) {
+                if(i%j==0) {
+                    flg=false;
+                }
+            }
+            if(flg==true) {
+                System.out.println(i);
+            }
+        }*/
+
+
+        /*public static void main(String[] args) {
+           int f1=1,f2=1,f;
+            System.out.println(f1);
+            System.out.println(f2);
+           int m=30;
+           for(int i=3;i<=30;i++) {
+              f=f1+f2;
+              f1=f2;
+              f2=f;
+               System.out.println(f);
+           }
+            }*/
+
+
+
+
+
