49b9bd34c3480d2c5a9a206aaf42b1ac19d349f8
zhengyudong0430 <2238642137@qq.com>
2020-03-20 23:31:55 +0800

    类与对象

---
+++ test-3-20/src/TestDome.java
@@ -0,0 +1,21 @@
+public class Person{
+
+    private String name = "Person";
+
+    int age=0;
+
+}
+
+public class Child extends Person{
+
+    public String grade;
+
+    public static void main(String[] args){
+
+        Person p = new Child();
+
+        System.out.println(p.name);
+
+    }
+
+}
