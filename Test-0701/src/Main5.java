4074d93a28b312a69a18270318c19a5833765df5
zhengyudong0430 <2238642137@qq.com>
2020-07-02 23:15:34 +0800

    Long.ToBinaryString,Long.parseLong

---
+++ Test-0701/src/Main5.java
@@ -0,0 +1,11 @@
+
+public class Main5 {
+    public static void main(String[] args) {
+       long a = 2;
+        String s = Long.toBinaryString(a);
+        System.out.println(s);
+        String s1 = "10";
+        long b = Long.parseLong(s1,2);
+        System.out.println(b);
+    }
+}
