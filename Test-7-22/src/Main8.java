23ccf0ecb17266e722e2a0fccc66fb3bb9094277
zhengyudong0430 <2238642137@qq.com>
2020-07-25 23:36:35 +0800

    水了一天

---
+++ Test-7-22/src/Main8.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int []power = new int[n];
+            for(int i = 0; i<n; i++) {
+                power[i] = scanner.nextInt();
+            }
+            int k = scanner.nextInt();
+            int d = scanner.nextInt();
+            
+        }
+    }
+}
---
+++ Test-7-22/src/Printer1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Printer1 {
+    public int[] clockwisePrint(int[][] mat, int n, int m) {
+        // write code here
+        return ;
+    }
+}
